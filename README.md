44ab7481f367411542c6d21c4fb136bb4d5bd51c
Hendyanto <hendyanto.inbox@gmail.com>
2016-10-29 06:49:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# README #
+
+Made for programming lesson for primary schoolers.
+
+This is a canvas implementation using fabric.js, to create rectangles and lines inside the canvas easily.
+
+Trying to make sure it is easy for them to understand codes from basics, by:
+
+- Using easy languages
+- Minimalizing setups
+- Using canvas so it is more interesting for the childrens
+
+## **Usage** ##
+
+`var one = new Box('red');`
+
+### available movements ###
+
+```javascript
+one.moveDown();
+one.moveUp();
+one.moveRight();
+one.moveLeft();
+```
---
+++ main.js
@@ -0,0 +1,125 @@
+
+var lines = {
+  canvas: null,
+  setCanvas: function(c){
+    this.canvas = c;
+  },
+  lines: [],
+  construct: function(c){
+    this.setCanvas(c);
+
+    this.canvas.add(this.createLine(20,0));
+  },
+  createLine: function(left,top){
+    var l = new fabric.Line([0,500,0,0], {
+      left: left,
+      top: top,
+      stroke: '#f49e42'
+    });
+    return l;
+  }
+};
+
+function Rectangle(canvas, colour='red', position={}){
+  var rectangle = {
+    canvas: null,
+    rect: null,
+    isMoving: false,
+    movementBuffer: [],
+    config: {
+      moveSpeed: 20,
+      colour: 'red'
+    },
+    setCanvas: function(c){
+      this.canvas = c;
+    },
+    setColour: function(c){
+      this.config.colour = c;
+    },
+    position: {
+      top: 0,
+      left: 0
+    },
+    computedPosition: {
+      top: 0,
+      left: 0
+    },
+    getPosition: function(){
+      return {
+        top: this.rect.top,
+        left: this.rect.left
+      };
+    },
+    setComputedTop: function(top){
+      this.computedPosition.top = top;
+    },
+    setComputedLeft: function(left){
+      this.computedPosition.left = left;
+    },
+    make: function(){
+      this.setCanvas(canvas);
+      this.rect = new fabric.Rect({
+        left: this.position.left,
+        top: this.position.top,
+        fill: this.config.colour,
+        width: 20,
+        height: 20
+      });
+      this.canvas.add(this.rect);
+      return this;
+    },
+    moveLeft: function(){
+      var left = this.computedPosition.left;
+      left -= this.config.moveSpeed;
+      this.computedPosition.left = left;
+
+      this.animate('left', left);
+    },
+    moveRight: function(){
+      var left = this.computedPosition.left;
+      left += this.config.moveSpeed;
+      this.computedPosition.left = left;
+
+      this.animate('left', left);
+    },
+    moveUp: function(){
+      var top = this.computedPosition.top;
+      top -= this.config.moveSpeed;
+      this.computedPosition.top = top;
+
+      this.animate('top', top);
+    },
+    moveDown: function(){
+      var top = this.computedPosition.top;
+      top += this.config.moveSpeed;
+      this.computedPosition.top = top;
+
+      this.animate('top', top);
+    },
+    animate: function(property, value){
+      this.movementBuffer.push({ "property": property, "value": value });
+    },
+    startAnimation: function(){
+      setInterval(function(){
+        if(!this.isMoving && this.movementBuffer.length > 0){
+          var movement = this.movementBuffer.shift();
+
+          this.isMoving = true;
+          this.rect.animate(movement.property, movement.value, {
+            duration: 500,
+            onChange: this.canvas.renderAll.bind(this.canvas),
+            onComplete: function(){
+              this.isMoving = false;
+            }.bind(this)
+          });
+        }
+      }.bind(this), 100);
+    }
+  };
+  var instance = rectangle;
+  instance.setCanvas(canvas);
+  instance.setColour(colour);
+  instance.make();
+  instance.startAnimation();
+  return instance;
+}
